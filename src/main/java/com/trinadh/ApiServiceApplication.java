17d316c3be68cb0bb505cf02a7a9333bdbef2429
Trinadh Rayala <3nadhmail@gmail.com>
2024-07-21 12:35:56 -0500

    Test Controller

--- src/main/java/com/trinadh/ApiServiceApplication.java
+++ src/main/java/com/trinadh/ApiServiceApplication.java
@@ -1,4 +1,4 @@
-package com.trinadh.api_service;
+package com.trinadh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/trinadh/controller/CourseController.java
+++ src/main/java/com/trinadh/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.trinadh.controller;
 
 import com.trinadh.dto.Course;
 import com.trinadh.service.CourseService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
 
     @Autowired
     private CourseService courseService;
@@ -61,6 +66,12 @@ public class CourseController {
     @GetMapping("/health")
     public String healthCheck() {
         return "UP";
+    }
+
+    @GetMapping("/greetings")
+    public String greetings(){
+        logger.info("Greetings endpoint hit");
+        return "Hello Trinadh";
     }
 
 //    @GetMapping("/welcome")
